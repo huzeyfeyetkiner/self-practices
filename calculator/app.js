a6abf967485c417d1801f35aacf29b902df24fe1
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-01-23 17:35:13 +0300

    basic calculator commited

--- calculator/app.js
+++ calculator/app.js
@@ -1,57 +1,73 @@
+//ONCLİCK İLE HTML ÜZERİNDE YAPILDIĞINDAN TÜM JS KODLARI YORUM SATIRI HALİNE GETİRİLDİ
 
+// let toplaDOM = document.querySelector("#topla")
+// let cikarDOM = document.querySelector("#cikar")
+// let carpDOM = document.querySelector("#carp")
+// let bolDOM = document.querySelector("#bol")
 
-let toplaDOM = document.querySelector("#topla")
-let cikarDOM = document.querySelector("#cikar")
-let carpDOM = document.querySelector("#carp")
-let bolDOM = document.querySelector("#bol")
-
-let deger1DOM = document.querySelector("#num1")
-let deger2DOM = document.querySelector("#num2")
-
-
-toplaDOM.addEventListener("click",topla)
-cikarDOM.addEventListener("click",cikar)
-carpDOM.addEventListener("click",carp)
-bolDOM.addEventListener("click",bol)
+// let deger1DOM = document.querySelector("#num1")
 
 
 
-let sonucDOM = document.querySelector("#sonuc")
+// toplaDOM.addEventListener("click",topla)
+// cikarDOM.addEventListener("click",cikar)
+// carpDOM.addEventListener("click",carp)
+// bolDOM.addEventListener("click",bol)
 
 
-function topla() {    
-    let sonuc = parseInt(deger1DOM.value) + parseInt(deger2DOM.value)
-    sonucDOM.value = sonuc
-    console.log(sonuc);
-}
 
-function cikar() {
-    let sonuc = parseInt(deger1DOM.value) - parseInt(deger2DOM.value)
-    sonucDOM.value = sonuc
-    console.log(sonuc);
-}
+// let sonucDOM = document.querySelector("#sonuc")
+// let sonuc = 0
 
-function carp() {
-    let sonuc = parseInt(deger1DOM.value) * parseInt(deger2DOM.value)
-    sonucDOM.value = sonuc
-    console.log(sonuc);
-}
+// deger1DOM.value = 0;
 
-function bol() {
-    let sonuc = parseInt(deger1DOM.value) / parseInt(deger2DOM.value)
-    sonucDOM.value = sonuc
-    console.log(sonuc);
-}
 
-let resetDOM = document.querySelector("#reset")
+// function topla() {    
+//     sonuc += parseInt(deger1DOM.value) 
+//     sonucDOM.value = sonuc
+    
+//     deger1DOM.value = ""
+// }
 
-resetDOM.addEventListener("click",reset)
+// function cikar() {
+//     sonuc -= parseInt(deger1DOM.value) 
+//     sonucDOM.value = sonuc
+    
+//     deger1DOM.value = ""
+// }
 
-function reset(){
-    deger1DOM.value = ""
-    deger2DOM.value = ""
-    sonucDOM.value = ""
-}
+// function carp() {
+//     sonuc *= parseInt(deger1DOM.value) 
+//     sonucDOM.value = sonuc
+    
+//     deger1DOM.value = ""
+// }
 
+// function bol() {
+//     sonuc /= parseInt(deger1DOM.value) 
+//     sonucDOM.value = sonuc
+    
+//     deger1DOM.value = ""
+// }
+
+// let resetDOM = document.querySelector("#reset")
+
+// resetDOM.addEventListener("click",reset)
+
+// function reset(){
+//     deger1DOM.value = ""
+//     deger2DOM.value = ""
+//     sonucDOM.value = ""
+// }
+
+
+
+// let btnNumberDOM = document.querySelectorAll(".btn-number")
+
+// btnNumberDOM.addEventListener("click",giveInput)
+
+// function giveInput(){
+//     deger1DOM.value += btnNumberDOM.value
+// }
 
 

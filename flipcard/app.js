c74478702a45f0b7e885354dfd8d3f5ebbd08044
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-01-24 19:02:51 +0300

    flipcard added

---
+++ flipcard/app.js
@@ -0,0 +1,57 @@
+const vocabulary = [
+
+    {
+        english : "Blue",
+        turkish : "Mavi"
+    },
+    {
+        english : "Teacher",
+        turkish : "Öğretmen"
+    },
+    {
+        english : "Computer",
+        turkish : "Bilgisayar"
+    },
+    {
+        english : "Phone",
+        turkish : "Telefon"
+    },
+    {
+        english : "Glass",
+        turkish : "Cam"
+    },
+    {
+        english : "Tomato",
+        turkish : "Domates"
+    },
+    {
+        english : "Paper",
+        turkish : "Kağıt"
+    },
+    {
+        english : "School",
+        turkish : "Okul"
+    },
+
+]
+
+const flipcards = document.querySelector(".flipcards")
+
+for(let i =0;i<vocabulary.length;i++) {
+    let liDOM = document.createElement("li")
+    liDOM.classList.add("flipcard")
+    liDOM.innerHTML = `<h3 class="flipcard-head">${vocabulary[i].turkish}</h3> 
+                       <p class="flipcard-body">${vocabulary[i].english}</p>
+    `
+    flipcards.appendChild(liDOM)
+
+}
+
+const fpDOM = document.querySelectorAll(".flipcard")
+
+fpDOM.forEach((item) => {
+    item.addEventListener("click",() => {
+        item.classList.toggle("clicked")
+    })
+})
+
